0820174712c98c9d2d870834d7906d384320858e
Shishir Bychapur <shishir.bychapur@thoughtworks.com>
2026-09-02 00:10:25 +0800

    refactor: use error + loading + not-found next.js feature

---
+++ src/app/members/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+import Alert from "@/src/components/alert/alert";
+
+export default function NotFound() {
+  return (
+    <Alert title="Not Found!" message="The requested member was not found." />
+  );
+}
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,12 +1,16 @@
+import { Member } from "@/src/types/member";
 import { AllMembers } from "@/src/components/members/all-members";
 
-const Members = () => {
+const Members = async () => {
+  const response = await fetch(process.env.URL + "/api/members");
+  const data: { members: Member[] } = await response.json();
+
   return (
     <div>
       <h1 className="mx-2 max-w-xs text-3xl font-semibold leading-10 tracking-tight">
         Members
       </h1>
-      <AllMembers />
+      <AllMembers members={data.members} />
     </div>
   );
 };
