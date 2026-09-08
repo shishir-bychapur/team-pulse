9e3f928eb5b46d86452eb2a57b798de6c17234fc
Shishir Bychapur <shishir.bychapur@thoughtworks.com>
2026-09-08 22:58:06 +0800

    feat: setup prisma

---
+++ prisma/migrations/20260908094511_init/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Member" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "timezone" TEXT NOT NULL,
+    "roleId" TEXT NOT NULL,
+    CONSTRAINT "Member_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Update" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "memberId" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "mood" TEXT NOT NULL,
+    CONSTRAINT "Update_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "Member" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ActionItem" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "dueDate" TEXT NOT NULL,
+    CONSTRAINT "ActionItem_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Member" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
